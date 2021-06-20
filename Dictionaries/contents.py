9c1e884a41c91c87c1746c5d9ea460018823fdc4
Ziya Alim <ziyabay135@gmail.com>
2021-06-21 01:42:51 +0300

    update

--- Dictionaries/contents.py
+++ Dictionaries/contents.py
@@ -1,69 +1,66 @@
 pantry = {
-    "chicken" : 500,
-    "lemon" : 2,
-    "cumin" : 24,
-    "paprika" : 18,
-    "chilli powder" : 7,
-    "yogurt" : 300,
-    "oil" : 450,
-    "onion" : 5,
-    "garlic" : 9,
-    "ginger" : 2,
-    "tomato puree" : 125,
-    "almonds" : 75,
-    "rice" : 500,
-    "coriander" : 20,
-    "lime" : 3,
-    "pepper" : 8,
-    "egg" : 6,
-    "pizza" : 2,
-    "spam" : 1, 
+    "chicken": 500,
+    "lemon": 2,
+    "cumin": 24,
+    "paprika": 18,
+    "chilli powder": 7,
+    "yogurt": 300,
+    "oil": 450,
+    "onion": 5,
+    "garlic": 9,
+    "ginger": 2,
+    "tomato puree": 125,
+    "almonds": 75,
+    "rice": 500,
+    "coriander": 20,
+    "lime": 3,
+    "pepper": 8,
+    "egg": 6,
+    "pizza": 2,
+    "spam": 1,
 }
 
 recipes = {
-    "Butter chicken": [
-        "chicken",
-        "lemon",
-        "cumin",
-        "paprika",
-        "chilli powder",
-        "yogurt",
-        "oil",
-        "onion",
-        "garlic",
-        "ginger",
-        "tomato puree",
-        "almonds",
-        "rice"
-    ],
-    "Chicken and chips": [
-        "chicken",
-        "potatoes",
-        "salt",
-        "malt vinegar",
-    ],
-    "Pizza": [
-        "pizza"
-    ],
-    "Egg sandwich": [
-        "egg",
-        "bread",
-        "butter",
-    ],
-    "Beans on toast": [
-        "beans",
-        "bread",
-    ],
-    "Spam a la tin":[
-        "spam",
-        "tin opener",
-        "spoon",
-    ],    
+    "Butter chicken": {
+        "chicken": 750,
+        "lemon": 1,
+        "cumin": 1,
+        "paprika": 1,
+        "chilli powder": 2,
+        "yogurt": 250,
+        "oil": 50,
+        "onion": 1,
+        "garlic": 2,
+        "ginger": 3,
+        "tomato puree": 240,
+        "almonds": 25,
+        "rice": 360,
+        "coriander": 1,
+        "lime": 1,
+    },
+    "Chicken and chips": {
+        "chicken": 100,
+        "potatoes": 3,
+        "salt": 1,
+        "malt vinegar": 5,
+    },
+    "Pizza": {
+        "pizza": 1,
+    },
+    "Egg sandwich": {
+        "egg": 2,
+        "bread": 80,
+        "butter": 10,
+    },
+    "Beans on toast": {
+        "beans": 1,
+        "bread": 40,
+    },
+    "Spam a la tin": {
+
+
+        "spam": 1,
+        "tin opener": 1,
+        "spoon": 1,
+    },
 }
-
-# for name, ingrediant in recipes.items():
-#     print(name, ingrediant)
-
-# recipes["Spam a la tin"] = ["it sucks"]
-# for name, ingrediant in recipes.items():
-#     print(name, ingrediant)
--- Dictionaries/meal_planner.py
+++ Dictionaries/meal_planner.py
@@ -4,28 +4,28 @@ from contents import pantry, recipes
 display_dict = {}
 for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
-    
+
 while True:
     # Display a menu of the recipes we know how to cook
     print("Please choose your recipe")
     print("-------------------------")
     for key, value in display_dict.items():
         print(f"{key} - {value}")
-        
+
     choice = input(": ")
 
     if choice == "0":
         break
-        
     elif choice in display_dict:
-        selected_items = display_dict[choice]
-        print(f"You have selected {selected_items}")
-        print("Checking ingredients...\nThey are: ")
-        ingredients = recipes[selected_items]
+        selected_item = display_dict[choice]
+        print(f"You have selected {selected_item}")
+        print("checking ingredients ...")
+        ingredients = recipes[selected_item]
         print(ingredients)
-        for food_item in ingredients:
-            if food_item in pantry:
+        for food_item, required_quantity in ingredients.items():
+            quantity_in_pantry = pantry.get(food_item, 0) # the use of get might seem a litte confusing, but it is just dict[key], and the 0 there is for default value
+            if required_quantity <= quantity_in_pantry:
                 print(f"\t{food_item} OK")
             else:
-                print(f"\tYou don't have a necessary ingredient: {food_item}")
-                
+                quantity_to_buy = required_quantity - quantity_in_pantry
+                print(f"\tYou need to buy {quantity_to_buy} of {food_item}")
