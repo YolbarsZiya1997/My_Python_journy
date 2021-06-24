cb7d18c5ab92c649d34a5fff0f31287b4862a975
Ziya Alim <ziyabay135@gmail.com>
2021-06-24 17:35:40 +0300

    Update

---
+++ Dictionaries/my_deepcopy.py
@@ -0,0 +1,17 @@
+from contents import recipes
+
+def my_deepcopy(d: dict) -> dict:
+    copied_dict = {}
+    for keys, values in d.items():
+        new_values = values.copy() # this is the line that makes the difference
+        # when you don't use the copy() method the data is not stored separately
+        # when copy() is used, memory opens a new storing place for it
+        copied_dict[keys] = new_values
+    return copied_dict
+
+
+recipes_copy = my_deepcopy(recipes)
+recipes_copy["Butter chicken"]["ginger"] = 300
+print(recipes_copy["Butter chicken"]["ginger"])
+print(recipes["Butter chicken"]["ginger"])
+        
