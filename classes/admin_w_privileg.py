43e3eca852bdb59b717ca172c19eb3c470ec8c10
YolbarsZiya1997 <ziyabay135@gmail.com>
2022-04-21 21:49:50 +0300

    update

---
+++ classes/admin_w_privileg.py
@@ -0,0 +1,40 @@
+class User:
+    "Create an user profile "
+    def __init__(self, user_name, first_name, last_name, email, location):
+        self.user_name  = user_name
+        self.first_name = first_name
+        self.last_name  = last_name
+        self.email      = email
+        self.location   = location
+        self.login_attempts = 0
+    
+        
+    def descrine_user(self):
+        print(f"\n{self.first_name} {self.last_name}")
+        print(f"User name: {self.user_name}")
+        print(f"Email Address: {self.email}")
+        print(f"Location: {self.location}")
+        
+    def greet_user(self):
+        print(f"\nWelcome back {self.user_name}")
+    
+    def read_attempts(self):
+        print(f"Attempt to login: {self.login_attempts}")
+        
+    def increment_login_attempts(self, attempts):
+        self.login_attempts += attempts
+        
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        
+class Admin(User):
+    """ Create an admin user"""
+    def __init__(self, user_name, first_name, last_name, email, location):
+        """Initialize the Admin class"""
+        super().__init__(user_name, first_name, last_name, email, location)
+        self.privileges = ["can add post", "can delete post", "can ban user"]
+        
+    def show_privileges(self):
+        print("The privileges are:")
+        for priv in self.privileges:
+            print(f"- {priv}")
--- classes/electric_car.py
+++ classes/electric_car.py
@@ -42,6 +42,15 @@ class Battery:
     def describe_battery(self):
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
+    
+    def upgrade_battery(self):
+        print(f"Current battery size... {self.battery_size}")
+        if self.battery_size < 100:
+            print("Upgrade needed...")
+            print("Upgrading...")
+            self.battery_size = 100
+            print(f"Upgrade complete. Current battery size is {self.battery_size}")
+            
         
     def get_range(self):
         """Print a statement about the range this battery provides."""
@@ -73,6 +82,8 @@ class ElectricCar(Car):         # The child class
 my_tesla = ElectricCar("Tesla", "Model s", 2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()     
-my_tesla.battery.get_range()# it has the instances battery
-
+my_tesla.battery.get_range()     # it has the instances battery
+my_tesla.battery.upgrade_battery()
+print("*" * 80)
+my_tesla.battery.get_range()
 
