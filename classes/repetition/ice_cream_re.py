dff01e6ef76b565d4725157213a12a04ae23d042
YolbarsZiya1997 <ziyabay135@gmail.com>
2022-05-02 01:38:52 +0300

    update

--- classes/repetition/ice_cream_re.py
+++ classes/repetition/ice_cream_re.py
@@ -1,30 +1,33 @@
 class Restaurant:
-    """ Describe a restaurant""" 
+    """ Describe a restaurant"""
+
     def __init__(self, name, cuisine_type):
         self.name = name
         self.cuisine_type = cuisine_type
-        
+
     def describe_restaurant(self):
         """ Describe the restaurant"""
-        discrition = f"{self.name} serves wonderful {self.cuisine_type} cuisine"
-        print(f"{discrition}")
-        
+        description = f"{self.name} serves wonderful {self.cuisine_type} cuisine"
+        print(f"{description}")
+
     def open_restaurant(self):
         """ Invites the guests in"""
         invitation = f"{self.name} is opened, come on in"
         print(f'{invitation}')
-        
+
+
 class IceCreamStand(Restaurant):
     def __init__(self, name, cuisine_type='ice_cream'):
         """Initialize the class"""
         super().__init__(name, cuisine_type)
         self.flavor = []
-        
+
     def display_flavor(self):
         print(f'Those are the flavors:')
         for flavor in self.flavor:
             print(f'\t-{flavor}')
-        
+
+
 my_fav = IceCreamStand('Zafer', 'ice_cream')
-my_fav.flavor = ['Cream', 'Chocolate', 'Cramberry', 'Lemon', 'Strawberry']
+my_fav.flavor = ['Cream', 'Chocolate', 'Cranberry', 'Lemon', 'Strawberry']
 my_fav.display_flavor()
