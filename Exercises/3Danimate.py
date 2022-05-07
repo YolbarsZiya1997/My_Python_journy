9195ef382983f9c25ca03cbac4607cef81435148
Ziya Alim <ziyabay135@gmail.com>
2022-05-07 18:10:59 +0300

    update

--- Exercises/3Danimate.py
+++ Exercises/3Danimate.py
@@ -4,7 +4,7 @@
 from vpython import *  # import graphics routines
 
 xmax = 201  # number of x, y points
-scene = canvas(x=0, y=0, width=500, height=500,
+scene = display(x=0, y=0, width=500, height=500,
                 title='sin(6pi*x/201-t)', background=(1., 1., 1.0),
                 forward=(-0.6, -0.5, -1))
 sinWave = curve(x=range(0, xmax), color=color.yellow, radius=4.5)
